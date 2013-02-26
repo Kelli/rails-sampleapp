ddfece6534c1f80fd866d6da2839cc4f47cc6a9d
Kelli Ireland <irelandk@upmc.edu>
2013-02-26 16:40:09 -0500

    Updating the paths

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,54 +6,54 @@ describe "StaticPages" do
   
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
     end
     
     it "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',:text => "#{base_title}")
+      visit root_path
+      page.should have_selector('title', text: "#{base_title}")
     end
     
     it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home|')
+      visit root_path
+      page.should_not have_selector('title', text: '| Home|')
     end
   end
   
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title',:text => "#{base_title} | Help")
+      visit help_path
+      page.should have_selector('title', text: "#{base_title} | Help")
     end
   end
   
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
+    it "should have the content 'About'" do
+      visit about_path
+      page.should have_content('About')
     end
     
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('title',:text => "#{base_title} | About Us")
+    it "should have the title 'About'" do
+      visit about_path
+      page.should have_selector('title', text: "#{base_title} | About")
     end
   end
   
   describe "Contact Us" do
-    it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
-      page.should have_content("Contact Us")
+    it "should have the content 'Contact'" do
+      visit contact_path
+      page.should have_content("Contact")
     end
     
-    it "should have the title 'Contact Us'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title',:text => "#{base_title} | Contact Us")
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title', text: "#{base_title} | Contact")
     end
   end
   
